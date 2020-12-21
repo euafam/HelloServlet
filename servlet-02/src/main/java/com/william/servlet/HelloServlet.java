cf46a8144242394d2468f4cd9c7fc673b7c51ab6
euafam <williamhansson@icloud.com>
2020-12-21 11:22:41 +0800

    servletcontext

---
+++ servlet-02/src/main/java/com/william/servlet/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.william.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ServletContext context = this.getServletContext();
+
+        String username = "qinjiang";
+        context.setAttribute("username",username);
+
+        System.out.println("Hello");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
